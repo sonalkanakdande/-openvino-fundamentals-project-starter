256f22d918b790c70e642136614b0b11f05df676
Sonal <44235033+sonalkanakdande@users.noreply.github.com>
2020-05-16 15:30:25 +0800

    Update main.py
    Correct MQTT Port to resolve connectivity issue

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ from inference import Network
 HOSTNAME = socket.gethostname()
 IPADDRESS = socket.gethostbyname(HOSTNAME)
 MQTT_HOST = IPADDRESS
-MQTT_PORT = 3001
+MQTT_PORT = 3000
 MQTT_KEEPALIVE_INTERVAL = 60
 
 
